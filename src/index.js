8489af2202bf0dfad6e7bf2c7f130fca73cf3d80
Christian Gabriel Cote <ccote@amberkinetics.com>
2024-07-09 12:32:15 -0700

    Added clipboard copy for contract and toast

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
@@ -44,6 +46,7 @@ function App() {
 
   return (
     <Container id="main-container">
+      <ToastContainer autoClose={5000} />
       <Header />
       <SocialsHeader mobileMode={isMobile} />
       <MainBanner />
@@ -69,6 +72,15 @@ function Header() {
         </header>
       </Col>
     </Row>
+  );
+}
+
+function ContractClip(props) {
+  const { mobileMode } = props;
+  return (
+    <section className="contractClip">
+      <p> {mobileMode ? shortenedEthAdd : ethAdd} </p>
+    </section>
   );
 }
 
@@ -160,7 +172,31 @@ function SocialsHeader(props) {
   return (
     <Row className="socialsHeader">
       <Col xs={12}>
-        <h4> {mobileMode ? shortenedEthAdd : ethAdd} </h4>
+        <section
+          className="contractClip"
+          onClick={() => {
+            navigator.clipboard.writeText(ethAdd);
+            toast.dismiss();
+            toast("Addy Copied! 🐶");
+          }}
+        >
+          <span> {mobileMode ? shortenedEthAdd : ethAdd} </span>
+          <svg
+            width="25px"
+            height="25px"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M17.5 14H19C20.1046 14 21 13.1046 21 12V5C21 3.89543 20.1046 3 19 3H12C10.8954 3 10 3.89543 10 5V6.5M5 10H12C13.1046 10 14 10.8954 14 12V19C14 20.1046 13.1046 21 12 21H5C3.89543 21 3 20.1046 3 19V12C3 10.8954 3.89543 10 5 10Z"
+              stroke="#000000"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </section>
         <div className="social-icon">
           <a href={twitter}>
             <img src={navIcon3} alt="" />
@@ -178,11 +214,11 @@ function SocialsHeader(props) {
 }
 
 function TopDivider() {
-  return <div class="skew-cc"></div>;
+  return <div className="skew-cc"></div>;
 }
 
 function BottomDivider() {
-  return <div class="skew-c"></div>;
+  return <div className="skew-c"></div>;
 }
 
 function Carousel() {
@@ -231,7 +267,7 @@ function BuyGuide() {
           <ul id="buy-list">
             <li>
               <span>1</span>
-              <div class="guideItem">
+              <div className="guideItem">
                 <h3>Create a wallet</h3>
                 <p>
                   Download Phantom or your wallet of choice from the app store
@@ -240,7 +276,7 @@ function BuyGuide() {
                 </p>
                 <a
                   href="https://phantom.app/"
-                  class="itemBtn"
+                  className="itemBtn"
                   style={{
                     boxShadow: "-5px 6px 0 0 #3b325b",
                     backgroundColor: "#ab9ff2",
@@ -248,15 +284,15 @@ function BuyGuide() {
                 >
                   PHANTOM
                   <svg
-                    class="css-1a1mo8u e18osog01"
+                    className="css-1a1mo8u e18osog01"
                     width="35"
                     height="30"
                     viewBox="65 30 30 120"
                     fill="none"
                   >
                     <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
+                      fillRule="evenodd"
+                      clipRule="evenodd"
                       d="M89.1138 112.613C83.1715 121.719 73.2139 133.243 59.9641 133.243C53.7005 133.243 47.6777 130.665 47.6775 119.464C47.677 90.9369 86.6235 46.777 122.76 46.7764C143.317 46.776 151.509 61.0389 151.509 77.2361C151.509 98.0264 138.018 121.799 124.608 121.799C120.352 121.799 118.264 119.462 118.264 115.756C118.264 114.789 118.424 113.741 118.746 112.613C114.168 120.429 105.335 127.683 97.0638 127.683C91.0411 127.683 87.9898 123.895 87.9897 118.576C87.9897 116.642 88.3912 114.628 89.1138 112.613ZM115.936 68.7103C112.665 68.7161 110.435 71.4952 110.442 75.4598C110.449 79.4244 112.689 82.275 115.96 82.2693C119.152 82.2636 121.381 79.4052 121.374 75.4405C121.367 71.4759 119.128 68.7047 115.936 68.7103ZM133.287 68.6914C130.016 68.6972 127.786 71.4763 127.793 75.4409C127.8 79.4055 130.039 82.2561 133.311 82.2504C136.503 82.2448 138.732 79.3863 138.725 75.4216C138.718 71.457 136.479 68.6858 133.287 68.6914Z"
                       fill="currentColor"
                     ></path>
@@ -266,7 +302,7 @@ function BuyGuide() {
             </li>
             <li>
               <span>2</span>
-              <div class="guideItem">
+              <div className="guideItem">
                 <h3>Buy SOL</h3>
                 <p>
                   Have SOL in your wallet to switch to {ticker} If you dont have
@@ -277,7 +313,7 @@ function BuyGuide() {
             </li>
             <li>
               <span>3</span>
-              <div class="guideItem">
+              <div className="guideItem">
                 <h3>Go to a DEX</h3>
                 <p>
                   Connect to Raydium. Go raydium.io in google chrome or on the
@@ -289,14 +325,14 @@ function BuyGuide() {
             </li>
             <li>
               <span>4</span>
-              <div class="guideItem">
+              <div className="guideItem">
                 <h3>SWAP</h3>
                 <p>Switch SOL for {ticker}</p>
               </div>
             </li>
             <li>
               <div>
-                <a href={buylink} id="buy-btn" class="btn">
+                <a href={buylink} id="buy-btn" className="btn">
                   Get $POP!
                 </a>
               </div>
@@ -485,97 +521,18 @@ function CreateYours() {
   );
 }
 
-function FallingPop() {
-  return (
-    <div class="snowflakes">
-      <div class="snowflake">
-        <img
-          src="https://cinni.net/images/favi/p/poke_61.gif"
-          style="width:20px;"
-        />
-      </div>
-      <div class="snowflake">
-        <img
-          src="https://cinni.net/images/favi/p/poke_61.gif"
-          style="width:20px;"
-        />
-      </div>
-      <div class="snowflake">
-        <img
-          src="https://cinni.net/images/favi/p/poke_61.gif"
-          style="width:20px;"
-        />
-      </div>
-      <div class="snowflake">
-        <img
-          src="https://cinni.net/images/favi/p/poke_61.gif"
-          style="width:20px;"
-        />
-      </div>
-      <div class="snowflake">
-        <img
-          src="https://cinni.net/images/favi/p/poke_61.gif"
-          style="width:20px;"
-        />
-      </div>
-      <div class="snowflake">
-        <img
-          src="https://cinni.net/images/favi/p/poke_61.gif"
-          style="width:20px;"
-        />
-      </div>
-      <div class="snowflake">
-        <img
-          src="https://cinni.net/images/favi/p/poke_61.gif"
-          style="width:20px;"
-        />
-      </div>
-      <div class="snowflake">
-        <img
-          src="https://cinni.net/images/favi/p/poke_61.gif"
-          style="width:20px;"
-        />
-      </div>
-      <div class="snowflake">
-        <img
-          src="https://cinni.net/images/favi/p/poke_61.gif"
-          style="width:20px;"
-        />
-      </div>
-      <div class="snowflake">
-        <img
-          src="https://cinni.net/images/favi/p/poke_61.gif"
-          style="width:20px;"
-        />
-      </div>
-      <div class="snowflake">
-        <img
-          src="https://cinni.net/images/favi/p/poke_61.gif"
-          style="width:20px;"
-        />
-      </div>
-      <div class="snowflake">
-        <img
-          src="https://cinni.net/images/favi/p/poke_61.gif"
-          style="width:20px;"
-        />
-      </div>
-    </div>
-  );
-}
-
 function TickerTape() {
   return (
-    <div class="ticker-wrap">
-      <div class="ticker">
-        <div class="ticker__item">#PopOnSOL</div>
-        <div class="ticker__item">#PopOnSOL</div>
-        <div class="ticker__item">#PopOnSOL</div>
-        <div class="ticker__item">#PopOnSOL</div>
-        <div class="ticker__item">#PopOnSOL</div>
-        <div class="ticker__item">#PopOnSOL</div>
-        <div class="ticker__item">#PopOnSOL</div>
-        <div class="ticker__item">#PopOnSOL</div>
+    <div className="ticker-wrap">
+      <div className="ticker">
+        <div className="ticker__item">#PopOnSOL</div>
+        <div className="ticker__item">#PopOnSOL</div>
+        <div className="ticker__item">#PopOnSOL</div>
+        <div className="ticker__item">#PopOnSOL</div>
+        <div className="ticker__item">#PopOnSOL</div>
+        <div className="ticker__item">#PopOnSOL</div>
+        <div className="ticker__item">#PopOnSOL</div>
+        <div className="ticker__item">#PopOnSOL</div>
       </div>
     </div>
   );
